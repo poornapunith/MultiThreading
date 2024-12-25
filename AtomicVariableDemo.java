dac8868df415810d85bf47a015b146cf3df2c9e8
poornapunith <poornachandra813@gmail.com>
2024-12-25 10:24:11 +0530

    Atomic Variables Demo added

---
+++ AtomicVariableDemo.java
@@ -0,0 +1,28 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicVariableDemo {
+//    public static int counter =0;
+    public static AtomicInteger counter = new AtomicInteger();
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    counter.incrementAndGet();
+                }
+
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    counter.incrementAndGet();
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(counter);
+    }
+}
