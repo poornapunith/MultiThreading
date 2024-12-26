ada838d29d842e210e848c85f203f2933d32b5ba
poornapunith <poornachandra813@gmail.com>
2024-12-26 09:52:08 +0530

    Callable Thread Demo modified with Timeout

--- CallableDemo.java
+++ CallableDemo.java
@@ -4,8 +4,8 @@ public class CallableDemo {
     public static void main(String[] args) {
         try (ExecutorService executor = Executors.newSingleThreadExecutor()) {
             Future<Integer> data = executor.submit(new CallableThread());
-            System.out.println("Received from Callable Thread : "+data.get());
-        } catch (ExecutionException | InterruptedException e) {
+            System.out.println("Received from Callable Thread : "+data.get(5,TimeUnit.SECONDS));
+        } catch (ExecutionException | InterruptedException | TimeoutException e) {
             throw new RuntimeException(e);
         }
     }
@@ -15,6 +15,7 @@ class CallableThread implements Callable<Integer>
 {
     @Override
     public Integer call() throws Exception {
+        Thread.sleep(2000);
         return 5;
     }
 }
